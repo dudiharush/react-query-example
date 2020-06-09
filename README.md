8aa183c254616a3a242374ebf454860551662b64
dudiharush <davidharush@gmail.com>
2020-06-09 19:34:33 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # react-query-example
-Created with CodeSandbox
+
+An example usage of react with react-query for fetching and caching server side data
+
+each time you click the "Add" button, look at the dev-tools console and notice there is only one 'get' call to the server for data revalidation
---
+++ src/App.tsx
@@ -0,0 +1,53 @@
+import * as React from "react";
+import "./styles.css";
+import { server, GET_USERS_PATH } from "./server";
+import { useQuery, useMutation } from "react-query";
+
+const LastPersonFirstName = () => {
+  const { status, data: people, error } = useQuery(GET_USERS_PATH, server.get);
+  if (status === "error") return <div>failed to load. ${error?.message}</div>;
+  if (status === "loading") return <div>loading...</div>;
+  return (
+    <div>last person's first name: {people![people!.length - 1].firstName}</div>
+  );
+};
+
+const LastPersonLastName = () => {
+  const { status, data: people, error } = useQuery(GET_USERS_PATH, server.get);
+  if (status === 'error') return <div>failed to load. ${error?.message}</div>;
+  if (!people) return <div>loading...</div>;
+  return (
+    <div>last person's last name: {people[people.length - 1].lastName}</div>
+  );
+};
+
+const addUserFetcher = ({ id }:{id: number}) => server.addById(id);
+
+const List = () => {
+  const { status, data: people, error } = useQuery(GET_USERS_PATH, server.get);
+  const [addUser] = useMutation(addUserFetcher)
+
+  const onClick = async () => {
+    const nextId = people!.length + 1;
+    addUser({id:nextId})
+  };
+
+
+  if (status === 'error') return <div>failed to load. ${error?.message}</div>;
+  if (status === 'loading') return <div>loading...</div>;
+  return (
+    <div className="App">
+      <h1>React and react-query</h1>
+      <LastPersonFirstName />
+      <LastPersonLastName />
+
+      {people!.map((p,index) => (
+        <div key={index}>{p.firstName}</div>
+      ))}
+      <button onClick={onClick}>add</button>
+    </div>
+  );
+};
+export default function App() {
+  return <List />;
+}
